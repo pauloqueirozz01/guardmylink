0b0cd06f93956af396f200ef371250c14451fdbb
Paulo Queiroz | IOS Dev <pauloqueirozzdev@gmail.com>
2026-04-02 17:34:59 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>GuardMyLink</Text>
+      <Text>Welcome to my first RN App</Text>
+    </View>
+  );
+}
