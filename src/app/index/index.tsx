6e44988364cf4c186624fa6e8436d2f236910381
Paulo Queiroz | IOS Dev <pauloqueirozzdev@gmail.com>
2026-04-02 22:23:29 -0300

    feat: add a header component tsx with materialIcons, and add a logo image

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,12 +1,21 @@
-import { Text, View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
+
+import { colors } from "@/styles/colors";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>GuardMYLink</Text>
-      <Text style={styles.subtitle}>in development...</Text>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity activeOpacity={0.6}>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -4,8 +4,19 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: 62,
+  },
+  logo: {
+    height: 32,
+    width: 38,
+  },
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 32,
   },
   title: {
     fontSize: 24,
